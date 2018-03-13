4e0302b545c8f7eb3ce1e0842c8c7cd0e8f9e18b
savoptik <sav70792@gmail.com>
2018-03-13 20:37:14 +0300

    добавлены файлы для дочернего класса.

---
+++ BinarizationManualWelectionThreshold.cpp
@@ -0,0 +1,9 @@
+//
+//  BinarizationManualWelectionThreshold.cpp
+//  Convert to a grayscale, binary images
+//
+//  Created by Артём Семёнов on 13.03.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include "BinarizationManualWelectionThreshold.hpp"
---
+++ BinarizationManualWelectionThreshold.hpp
@@ -0,0 +1,20 @@
+//
+//  BinarizationManualWelectionThreshold.hpp
+//  Convert to a grayscale, binary images
+//
+//  Created by Артём Семёнов on 13.03.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#ifndef BinarizationManualWelectionThreshold_hpp
+#define BinarizationManualWelectionThreshold_hpp
+
+#include <stdio.h>
+#include "grayScale.hpp"
+
+class BinarizationManualWelectionThreshold : grayScale {
+public:
+    <#member functions#>
+};
+
+#endif /* BinarizationManualWelectionThreshold_hpp */
