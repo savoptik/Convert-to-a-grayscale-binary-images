4f402640abf4cd451d4787b5309ab9113d7acd5c
savoptik <sav70792@gmail.com>
2018-03-13 13:46:38 +0300

    Initial Commit

---
+++ Convert to a grayscale, binary images/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Convert to a grayscale, binary images
+//
+//  Created by Артём Семёнов on 13.03.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
