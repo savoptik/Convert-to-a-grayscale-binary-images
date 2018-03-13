352cea8509a28c439dcc371452d7930ce420ad41
savoptik <sav70792@gmail.com>
2018-03-13 14:58:43 +0300

    Добавлено описание метода, переводящего  изображение в полутоновое.

--- Convert to a grayscale, binary images/grayScale.cpp
+++ Convert to a grayscale, binary images/grayScale.cpp
@@ -24,3 +24,16 @@ void grayScale::showGrayImage() {
     waitKey(); // ждём нажатия клавиши.
     destroyWindow("Gray image"); // уничтожаем окно.
 }
+
+void grayScale::conversionTOGrayScale() {
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            auto buf = image.at<Vec3b>(i, j); // получаем пиксель, разложенный на каналы.
+            int mid = (buf[0] + buf[1] + buf[2])/3; // вычисляем среднее.
+            // записываем среднее в каналы.
+            image.at<Vec3b>(i, j)[0] = mid;
+            image.at<Vec3b>(i, j)[1] = mid;
+            image.at<Vec3b>(i, j)[2] = mid;
+        }
+    }
+}
