193979e891f29dae5b813e394e730b3966415828
savoptik <sav70792@gmail.com>
2018-03-14 18:42:24 +0300

    Переделанн суперкласс.

---
+++ Convert to a grayscale, binary images/binaryConversions.cpp
@@ -0,0 +1,27 @@
+//
+//  binaryConversions.cpp
+//  Convert to a grayscale, binary images
+//
+//  Created by Артём Семёнов on 14.03.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include "binaryConversions.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+using namespace std;
+using namespace cv;
+
+void binaryConversions::conversionTOGrayScale() {
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            auto buf = image.at<Vec3b>(i, j); // получаем пиксель, разложенный на каналы.
+            int mid = (buf[0] + buf[1] + buf[2])/3; // вычисляем среднее.
+            // записываем среднее в каналы.
+            image.at<Vec3b>(i, j)[0] = mid;
+            image.at<Vec3b>(i, j)[1] = mid;
+            image.at<Vec3b>(i, j)[2] = mid;
+        }
+    }
+}
